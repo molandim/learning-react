087a5123bc7a338a1ae9f94fb1685a5fa0374eff
molandim <molandim@gmail.com>
2017-07-04 22:31:23 +0100

    using connect to create react component

--- src/containers/CountWidget.jsx
+++ src/containers/CountWidget.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { incrementCount } from '../actions/actions';
 import CountWidget from '../components/CountWidget';
 
-export default class CountWidgetContainer extends Component {
+/* old store.getState() */
+function mapStateToProps(state){
+  return {count: state.count};
+}
+/* old store.dispatch */
+function mapDispatchToProps(dispatch){
+  return {
+    //handleClick: ()=> {console.log('I was clicked')}
+    handleClick: ()=> {dispatch(incrementCount())}
+  }
+}
+
+let CountWidgetContainer = connect(mapStateToProps, mapDispatchToProps)(CountWidget);
+export default CountWidgetContainer;
+
+/*export default class CountWidgetContainer extends Component {
 
 constructor(props) {
     super(props);
@@ -11,7 +27,6 @@ constructor(props) {
       count: props.store.getState().count
     }
       console.log('this.state: ', this.state);
-
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -37,4 +52,4 @@ constructor(props) {
       return <CountWidget count={this.state.count} handleClick={this.handleClick} />
   }
 
-}
+}*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { App } from './app';
-
 import { createStore } from 'redux';
 import reducer from './reducers/main-reducers';
 
-let store = createStore(reducer);
+let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
-ReactDOM.render(<App store={store} />, document.getElementById('react'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App store={store} />
+    </Provider>,
+    document.getElementById('react')
+);
